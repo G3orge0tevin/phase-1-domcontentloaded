971e8b0b41479b79b88ae0a09667f4f6412265de
Tevin <georgetevin0@gmail.com>
2024-07-19 14:38:32 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const textEditor = document.getElementById('text');
+    textEditor.innerHTML = "This is really cool!";
+});
